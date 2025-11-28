fb39db3cdae82093f8f9c3424dd0013eaf9f5b8a
Maethichai Kobkam <maethichai.ko@inet.co.th>
2025-11-28 17:06:23 +0700

    adjust readme

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
+import { config } from "dotenv";
+config();
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+
+app.get("/", () => "Hello Elysia").listen(8080);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
---
+++ src/routes/auth/index.ts
@@ -0,0 +1,3 @@
+import { Elysia } from 'elysia'
+
+export const authIndex = new Elysia({ name: 'Controller.Name', prefix: '/auth' }) 
